f27bd4f93dce5c2646103b39a681694916f74d16
JoeDevelops <joe@joedevelops.com>
2016-09-09 14:40:13 -0700

    Added Super Minimal Template with basic HTML structure and links to Material Design Lite and style.css for our project.  We don't need to keep this, I just thought it would be nice to have a rough idea of an interface.

--- TitanConnect/routes/index.js
+++ TitanConnect/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TitanConnect' });
+  res.render('TChome');
 });
 
 router.get('/register/', function(req, res, next) {
