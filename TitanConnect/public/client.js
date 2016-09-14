a349b8689d934dfef656cfb6fced2ef9d803e1b3
JoeDevelops <joe@joedevelops.com>
2016-09-13 19:48:47 -0700

    Modified Register Template and DB interaction

--- TitanConnect/public/client.js
+++ TitanConnect/public/client.js
@@ -1,25 +1,53 @@
-$(document).ready(function(){{
+$(document).ready(function(){
     console.log('Client Loaded:');
+    $(".messageArea").hide();
+    $(".error").empty();
+    $(".success").empty();
+	$("#cnfuserpass").onchange = validatePassword;
+
+    var validatePassword = function(){
+        var pass2 = $("#userpass").val();
+        var pass1 = $("#cnfuserpass").val();
+        if(pass1 !== pass2)
+        	$("#notMatchError").text("Passwords Don't Match");
+        else
+        	$("#notMatchError").text("").show();
+        //empty string means no validation error
+    };
+
     $('#submitRegistration').click(function(){
-        console.log('Register Clicked.');
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var phone = $('#phone').val();
-        var userpass = $('#userpass').val();
-        var cnfuserpass = $('#cnfuserpass').val();
-        var user = {
-                name: name,
-                email: email,
-                phone: phone,
-                userpass: userpass,
-                cnfuserpass: cnfuserpass
-        };
-        console.log("User: ", user);
-        if (verifyRegister(user)){
-            // submit to DB
+        var pw1, pw2;
+        pw1 = $('#userpass').val();
+        pw2 = $('#cnfuserpass').val();
+        if(pw1 === pw2 && pw1 !== ""){
+            console.log('Register Clicked.');
+            var name = $('#name').val();
+            var email = $('#email').val();
+            var phone = $('#phone').val();
+            var userpass = $('#userpass').val();
+            var cnfuserpass = $('#cnfuserpass').val();
+            var user = {
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    userpass: userpass,
+                    cnfuserpass: cnfuserpass
+            };
+            $.post( "/register", user, function( data ) {
+                //url = "/users/"+user.name;
+                //$( location ).attr("href", url);
+                console.log("Victory!");
+            })
+            .fail(function(err) {
+                $(".messageArea").show();
+                $(".error").append("Database Failure");
+            });
         }
         else{
-            // error message/redirect
+            console.log("Password matching error");
+            $(".messageArea").hide();
+            $(".error").append('Retype Passwords.');
+            $(".error").addClass('alert-box error');
         }
     });
 
@@ -32,11 +60,5 @@ $(document).ready(function(){{
             password: password
         }
         console.log("User: ", user);
-        if (verifyLogin(user)){
-            // submit to DB
-        }
-        else{
-            // error message/redirect
-        }
     });
-}});
+});
--- TitanConnect/routes/index.js
+++ TitanConnect/routes/index.js
@@ -14,67 +14,36 @@ router.get('/register/', function(req, res, next) {
   res.render('register');
 });
 
+router.post('/register/', function(req, res, next) {
+    if (validName(name) & validEmail(email)){
+        var user = new User({
+            userName: req.body.name,
+            email: req.body.email,
+            firstName: "N/A",
+            lastName: "N/A",
+            phone: req.body.phone,
+            password: req.body.password,
+        });
+        user.save(function(err, user){
+            if(err){
+                console.log(err);
+                res.render('register', {error:"Database Error"});
+            }
+            else{
+                res.render('login', {success:"Account Created."});
+            }
+        });
+    }
+    else{
+        res.render('register', {error:"Username or Email already on system."})
+    }
+});
+
 router.get('/login/', function(req, res, next) {
   res.render('login');
 });
 
-var addDemoUser = function(){
-    var user = new User({
-        userName: '111NEWDEMO',
-        email: '111NEWDEMOEMAIL@DEMO.COM',
-        password: 'example',
-        firstName: 'E',
-        lastName: 'Xample',
-        phone: '000-000-0000',
-        bio: 'example',
-        belongsTo: []
-    });
-    user.save(function(err) {
-        if (err) {
-            console.log(err);
-            throw err;
-        };
-
-    console.log('User saved successfully!');
-    });
-};
-
-var addDemoGroup = function(){
-    User.find({ userName: 'example' }, function(err, user) {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-        console.log('example user found!');
-        var group = new Group({
-            groupName: 'DEMOGROUP',
-            admin: user._id,
-            groupMembers: [ user._id ],
-            groupChat: [{
-                author: user._id,
-                comment: 'exampleGroup Created!',
-                timestamp: Date.now()
-            }],
-            description: 'An example group created for testing',
-            groupCreatedDate: Date.now()
-        });
-        group.save(function(err) {
-            if (err) {
-                console.log(err);
-                throw err;
-            };
-            console.log('Group saved successfully!');
-        });
-    });
-    Group.find({ groupName: 'exampleGroup' }, function(err, group) {
-        User.update(
-            { userName: 'NEWDEMO'},
-            { $push: { belongsTo: group._id }}, {upsert: true}
-        );
-    });
-};
-
-//addDemoUser();
-//addDemoGroup();
+var validName = function(){ return true; };
+var validEmail = function(){ return true; };
 
 module.exports = router;
